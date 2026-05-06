82da0531c761fb857de48f8e6acf27d1f523af60
nicolas0788 <nicolas.molinari0788@gmail.com>
2026-05-05 21:13:51 -0300

    avances 05/05

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://nicolas0788.github.io",
+  base: "/psicologia_en_linea",
+});
